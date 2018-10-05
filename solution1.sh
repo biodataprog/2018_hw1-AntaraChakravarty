7d076a81f2cf57cff3ce7daa9fc960d588b0cf50
AntaraChakravarty <43831922+AntaraChakravarty@users.noreply.github.com>
2018-10-04 21:07:17 -0700

    Update solution1.sh

--- solution1.sh
+++ solution1.sh
@@ -7,4 +7,5 @@ $ du -h Ecoli-vs-Yersinia.BLASTP.tab
 $ head -25 Ecoli-vs-Yersinia.BLASTP.tab
 $ tail -3 Ecoli-vs-Yersinia.BLASTP.tab
 $ wc -l Ecoli-vs-Yersinia.BLASTP.tab
+$ curl https://biodataprog.github.io/2018_programming-intro/data/Nc3H.expr.tab
 $ awk '{print $1, $6}' Nc3H.expr.tab > Nc20H.expr.sorted.tab
