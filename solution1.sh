27045ba5fffd0b7c8289077f56f5d20987e0a3d3
AntaraChakravarty <43831922+AntaraChakravarty@users.noreply.github.com>
2018-10-04 18:45:27 -0700

    Update solution1.sh

--- solution1.sh
+++ solution1.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-# code should go here to download file https://biodataprog.github.io/programming-intro/data/Ecoli-vs-Yersinia.BLASTP.tab.gz
-
-echo "hi"
+$ curl -O https://biodataprog.github.io/2018_programming-intro/data/Ecoli-vs-Yersinia.BLASTP.tab.gz
+$ du -h Ecoli-vs-Yersinia.BLASTP.tab.gz
+$ gunzip Ecoli-vs-Yersinia.BLASTP.tab.gz
+$ du -h Ecoli-vs-Yersinia.BLASTP.tab
+$ head -25 Ecoli-vs-Yersinia.BLASTP.tab
+$ tail -3 Ecoli-vs-Yersinia.BLASTP.tab
+$ wc -l Ecoli-vs-Yersinia.BLASTP.tab
