ff45db692eaf1e022788bb1bb5151f5108f522ee
AntaraChakravarty <43831922+AntaraChakravarty@users.noreply.github.com>
2018-10-04 23:18:39 -0700

    Update solution1.sh

--- solution1.sh
+++ solution1.sh
@@ -22,3 +22,31 @@ NCU09477 4618.97
 NCU06911 4129.64
 NCU05567 3832.14
 NCU02250 3738.11
+
+
+
+$ curl -O https://biodataprog.github.io/2018_programming-intro/data/codon_table.txt
+$ sort -dk2  codon_table.txt > codon_table.sorted.txt
+$ awk '{print $2}' codon_table.sorted.txt > codon.text
+$ sort|uniq -c codon.text
+   3 *
+   4 A
+   2 C
+   2 D
+   2 E
+   2 F
+   4 G
+   2 H
+   3 I
+   2 K
+   6 L
+   1 M
+   2 N
+   4 P
+   2 Q
+   6 R
+   6 S
+   4 T
+   4 V
+   1 W
+   2 Y
