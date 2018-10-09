7dbb15f1931f77fc82a9109138feb43f283249b7
AntaraChakravarty <43831922+AntaraChakravarty@users.noreply.github.com>
2018-10-09 15:22:04 -0700

    Update solution1.sh

--- solution1.sh
+++ solution1.sh
@@ -26,12 +26,21 @@ curl -O https://biodataprog.github.io/2018_programming-intro/data/Nc3H.expr.tab
 
 head -n 11 Nc3H.expr.sorted.tab
 
+#solution to homework 5
 
+curl -O https://biodataprog.github.io/2018_programming-intro/data/D_mel.63B12.gbk
 
+grep -c "     CDS" D_mel.63B12.gbk
 
+cut -f3 Ecoli-vs-Yersinia.BLASTP.tab | grep -c "100\.0"
+
+awk '$3 > 90 {print $1,$2,$3}' Ecoli-vs-Yersinia.BLASTP.tab | wc -l
+
+#solution to homework 6
 
 curl -O https://biodataprog.github.io/2018_programming-intro/data/codon_table.txt
-sort -dk2  codon_table.txt > codon_table.sorted.txt
-awk '{print $2}' codon_table.sorted.txt > codon.text
-sort|uniq -c codon.text
+
+sort -dk2| awk '{print $2}' codon_table.txt > codon_table.sorted.txt
+
+sort|uniq -c codon_table.sorted.txt
    
