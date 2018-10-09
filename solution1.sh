0d7760d0702ecbe7832eadc374d093be4874e50d
AntaraChakravarty <43831922+AntaraChakravarty@users.noreply.github.com>
2018-10-09 15:25:15 -0700

    Solution 5 saved

--- solution1.sh
+++ solution1.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# solution to question 1
 curl -O https://biodataprog.github.io/2018_programming-intro/data/Ecoli-vs-Yersinia.BLASTP.tab.gz
+
 du -h Ecoli-vs-Yersinia.BLASTP.tab.gz
 
+# solution to question 2
+
 gunzip Ecoli-vs-Yersinia.BLASTP.tab.gz
+
 du -h Ecoli-vs-Yersinia.BLASTP.tab
+
+# solution to question 3
 
 head -25 Ecoli-vs-Yersinia.BLASTP.tab
 
@@ -12,13 +19,15 @@ tail -3 Ecoli-vs-Yersinia.BLASTP.tab
 
 wc -l Ecoli-vs-Yersinia.BLASTP.tab
 
+# solution to question 4
+
 curl https://biodataprog.github.io/2018_programming-intro/data/Nc3H.expr.tab
 
 awk '{print $1, $6}' Nc3H.expr.tab > Nc20H.expr.sorted.tab
 
 sort -nrk2 Nc20H.expr.sorted.tab | head -10
 
-# correct solution to homework 4
+# correct solution to question 4
 
 curl -O https://biodataprog.github.io/2018_programming-intro/data/Nc3H.expr.tab
 
@@ -26,7 +35,7 @@ curl -O https://biodataprog.github.io/2018_programming-intro/data/Nc3H.expr.tab
 
 head -n 11 Nc3H.expr.sorted.tab
 
-#solution to homework 5
+#solution to question 5
 
 curl -O https://biodataprog.github.io/2018_programming-intro/data/D_mel.63B12.gbk
 
@@ -36,7 +45,7 @@ cut -f3 Ecoli-vs-Yersinia.BLASTP.tab | grep -c "100\.0"
 
 awk '$3 > 90 {print $1,$2,$3}' Ecoli-vs-Yersinia.BLASTP.tab | wc -l
 
-#solution to homework 6
+#solution to question 6
 
 curl -O https://biodataprog.github.io/2018_programming-intro/data/codon_table.txt
 
